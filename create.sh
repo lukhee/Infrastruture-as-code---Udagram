7d57f658092a140211c8768630af184b522f084c
balogun Lukman <lukheebalo@gmail.com>
2022-06-17 23:15:28 +0100

    chore: Adding NAT Gateway and NAT aattchement to vpc

--- create.sh
+++ create.sh
@@ -1,7 +1,2 @@
-#?/bin/sh
-aws cloudformation create-stack /
---stack-name $1 /
---template-body file://$2  /
---parameters file://$3 /
---capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" /
---region=us-east-1
+#!/bin/bash
+aws cloudformation create-stack --profile $1 --stack-name $2 --template-body file://$3  --parameters file://$4 --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=us-east-1
